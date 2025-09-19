041955c99bbe1cde3f969f827b76d5d37b8b8f0b
Kyler Dana <kdana@virtra.local>
2025-09-19 14:22:05 -0700

    Added Replication Toggle for Prod and outputs.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,18 @@ resource "aws_s3_bucket" "app" {
   tags = var.tags
 }
 
+# Versioning and Prod-Only Replication
+resource "aws_s3_bucket_versioning" "app" {
+  bucket = aws_s3_bucket.app.id 
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# When Replication = True
+resource "aws_s3_account_public_access_block" "app" {
+  block_public_acls = true 
+  block_public_policy = true 
+  restrict_public_buckets = true 
+  ignore_public_acls = true 
+}
